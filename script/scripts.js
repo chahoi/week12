4a9e07f73e343ca0b7698e7f57c76f7fab073cb3
Jake Choi <9822cj1@gmail.com>
2022-11-22 10:23:13 -0800

    alert once after page is loaded

--- script/scripts.js
+++ script/scripts.js
@@ -1,11 +1,39 @@
 const clickMeButton = document.querySelector(".click-me");
 
-// clickMeButton.addEventListener('click', )
+//It will remove the eventListener
+// function handleClickMeButton()
+// {
+//     alert("something");   
+// }
 
-clickMeButton.addEventListener('click',handleClickMeButton);
+// clickMeButton.addEventListener('click',handleClickMeButton);
 
+// clickMeButton.removeEventListener('click', handleClickMeButton);
+
+
+//It does not remove event listener
+// clickMeButton.addEventListener('click',function handleClickMeButton()
+// {
+//     alert("something");   
+// });
+
+// clickMeButton.removeEventListener('click', function handleClickMeButton()
+// {
+//     alert("something");   
+// });
+
+//Make it only alert once after page is loaded first time
+// clickMeButton.addEventListener('click',function handleClickMeButton()
+// {
+//     alert("something");   
+//     clickMeButton.removeEventListener('click', handleClickMeButton);
+// });
+
+//Another way to alert only once. .addEventListener('click',function,option);
 
 function handleClickMeButton()
 {
     alert("something");   
 }
+
+clickMeButton.addEventListener('click', handleClickMeButton, {once:true});
