0fef6430665834f15ec143c93fa2a309f3f43134
Jake Choi <9822cj1@gmail.com>
2022-11-22 10:10:20 -0800

    add eventListener

--- script/scripts.js
+++ script/scripts.js
@@ -2,10 +2,10 @@ const clickMeButton = document.querySelector(".click-me");
 
 // clickMeButton.addEventListener('click', )
 
+clickMeButton.addEventListener('click',handleClickMeButton);
+
+
 function handleClickMeButton()
 {
     alert("something");   
 }
-
-clickMeButton.addEventListener('click',handleClickMeButton);
-
