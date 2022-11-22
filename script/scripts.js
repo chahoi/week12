c638b7cb6082278d14da0f5e48bd9dd59ba55eb7
Jake Choi <9822cj1@gmail.com>
2022-11-22 09:56:48 -0800

    add button and make it alert using function

---
+++ script/scripts.js
@@ -0,0 +1,11 @@
+const clickMeButton = document.querySelector(".click-me");
+
+// clickMeButton.addEventListener('click', )
+
+function handleClickMeButton()
+{
+    alert("something");   
+}
+
+clickMeButton.addEventListener('click',handleClickMeButton);
+
